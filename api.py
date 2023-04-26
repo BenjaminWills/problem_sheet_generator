44e2f7987cb061152f0b398c5d445bdb52a95ddd
Benjaminwills <benjaminwills047@gmail.com>
2023-04-26 20:09:14 +0100

    add format topic abstraction

--- api.py
+++ api.py
@@ -1,13 +1,10 @@
-import os
 import shutil
 
-from flask import Flask, send_file, request, Response
-from io import BytesIO
-from werkzeug.wsgi import wrap_file
+from flask import Flask, send_file, request
 
 
 from problem_generator.generate_problems import generate_problem_sheet
-from problem_generator.utilities import zip_files
+from problem_generator.utilities import zip_files, format_topic
 
 
 app = Flask(__name__)
@@ -15,9 +12,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    b = BytesIO(b"blah blah blah")
-    w = wrap_file(file=b, environ=locals())
-    return Response(w, mimetype="text/plain", direct_passthrough=True)
+    return "Hello World"
 
 
 @app.route("/download")
@@ -25,7 +20,7 @@ def download():
     n_problems = request.args.get("n-problems", 5)
     topic = request.args.get("topic", "mathematics")
 
-    formatted_topic = topic.replace("-", "_")
+    formatted_topic = format_topic(topic)
 
     generate_problem_sheet(n_problems, formatted_topic)
 
@@ -39,7 +34,6 @@ def download():
     return send_file(
         f"{zip_path}.zip",
         as_attachment=True,
-        attachment_filename=f"{topic}_problems.zip",
     )
 
 
