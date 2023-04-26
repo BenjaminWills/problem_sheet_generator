34ef1f312d0d59a48f3887e15bd063d953002b78
Benjaminwills <benjaminwills047@gmail.com>
2023-04-26 22:02:26 +0100

    refactor code

--- frontend/problem-generation/src/App.js
+++ frontend/problem-generation/src/App.js
@@ -1,61 +1,11 @@
-import React, { useState } from 'react';
 import Navbar from './navbar/Navbar'
+import ApiForm from './api_form/ApiForm';
 
 function App() {
-  const [numberValue, setNumberValue] = useState('');
-  const [stringValue, setStringValue] = useState('');
-
-  const handleNumberChange = (event) => {
-    setNumberValue(event.target.value);
-  }
-
-  const handleStringChange = (event) => {
-    setStringValue(event.target.value);
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const data = {
-      number: numberValue,
-      string: stringValue
-    };
-
-    const api_url = `http://127.0.0.1:5000/download?=n-problems=${data.number}&topic=${data.string}`
-
-    console.log(api_url)
-
-    fetch(api_url)
-      .then(response => response.blob())
-      .then(blob => {
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'problems.zip';
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-        URL.revokeObjectURL(url);
-      })
-      .catch(error => console.error(error));
-  }
-
   return (
     <div>
       <Navbar></Navbar>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Enter a number:
-          <input type="number" value={numberValue} onChange={handleNumberChange} />
-        </label>
-        <br />
-        <label>
-          Enter a string:
-          <input type="text" value={stringValue} onChange={handleStringChange} />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+      <ApiForm></ApiForm>
     </div>
   );
 }
---
+++ frontend/problem-generation/src/api_form/ApiForm.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+
+function ApiForm() {
+    const [numberValue, setNumberValue] = useState('');
+  const [stringValue, setStringValue] = useState('');
+
+  const handleNumberChange = (event) => {
+    setNumberValue(event.target.value);
+  }
+
+  const handleStringChange = (event) => {
+    setStringValue(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      number: numberValue,
+      string: stringValue
+    };
+
+    const api_url = `http://127.0.0.1:5000/download?=n-problems=${data.number}&topic=${data.string}`
+
+    console.log(api_url)
+
+    fetch(api_url)
+      .then(response => response.blob())
+      .then(blob => {
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'problems.zip';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        URL.revokeObjectURL(url);
+      })
+      .catch(error => console.error(error));
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+        <label>
+          Enter a number:
+          <input type="number" value={numberValue} onChange={handleNumberChange} />
+        </label>
+        <br />
+        <label>
+          Enter a string:
+          <input type="text" value={stringValue} onChange={handleStringChange} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+  );
+}
+
+export default ApiForm;
