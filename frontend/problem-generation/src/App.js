09d66b9c2f68266d8ba1eca3253f3bb7d9001011
Benjaminwills <benjaminwills047@gmail.com>
2023-04-26 21:11:16 +0100

    current state of affairs

--- frontend/problem-generation/src/App.js
+++ frontend/problem-generation/src/App.js
@@ -1,7 +1,52 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
 function App() {
+  const [numberValue, setNumberValue] = useState('');
+  const [stringValue, setStringValue] = useState('');
+
+  const handleNumberChange = (event) => {
+    setNumberValue(event.target.value);
+  }
+
+  const handleStringChange = (event) => {
+    setStringValue(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      number: numberValue,
+      string: stringValue
+    };
+
+    const api_url = `http://127.0.0.1:5000/download?=n-problems=${data.number}&topic=${data.string}`
+    console.log(api_url)
+    axios.get(api_url, data)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   return (
-    <div className="App">
-      <h1>HELLO</h1>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Enter a number:
+          <input type="number" value={numberValue} onChange={handleNumberChange} />
+        </label>
+        <br />
+        <label>
+          Enter a string:
+          <input type="text" value={stringValue} onChange={handleStringChange} />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
