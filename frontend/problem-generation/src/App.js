7ba5bcbfb8776e4a01f548879ce58eccd7495d96
Benjaminwills <benjaminwills047@gmail.com>
2023-04-26 23:24:30 +0100

    update base app

--- frontend/problem-generation/src/App.js
+++ frontend/problem-generation/src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './navbar/Navbar';
 import ApiForm from './api_form/ApiForm';
+import Selector from './Selector/Selector';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Selector/>
       <ApiForm />
     </div>
   );
