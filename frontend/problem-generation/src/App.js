e5827e00c8e28e5e17d9bdac71ba8bdf7ff92b19
Benjaminwills <benjaminwills047@gmail.com>
2023-04-28 19:35:19 +0100

    remove selector from app

--- frontend/problem-generation/src/App.js
+++ frontend/problem-generation/src/App.js
@@ -1,13 +1,11 @@
 import Navbar from './navbar/Navbar';
 import ApiForm from './api_form/ApiForm';
-import Selector from './Selector/Selector';
 
 function App() {
   return (
     <div>
       <Navbar />
       <br />
-      <Selector />
       <ApiForm />
     </div>
   );
