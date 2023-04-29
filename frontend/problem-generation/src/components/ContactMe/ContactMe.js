9de2ed292d606e2e14131d2f6809b59d98c446b6
Benjaminwills <benjaminwills047@gmail.com>
2023-04-29 17:04:42 +0100

    update contact me page

--- frontend/problem-generation/src/components/ContactMe/ContactMe.js
+++ frontend/problem-generation/src/components/ContactMe/ContactMe.js
@@ -9,22 +9,27 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 function ContactMe() {
   return (
-    <div className="icon-container">
-      <div className="icon">
-        <a href="https://github.com/BenjaminWills" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} style={{ color: 'black' }} className="contact-icon" /></a>
-      </div>
-      <div className="icon">
-        <a href="https://www.linkedin.com/in/benjamin-wills-b22887220/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} style={{ color: '#0077b5' }} className="contact-icon" /></a>
-      </div>
-      <div className="icon">
-        <a href="https://www.instagram.com/ben_wills123/?hl=en-gb" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faInstagram} className="contact-icon" /></a>
-      </div>
-      <div className="icon">
-        <a href="https://twitter.com" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} className="contact-icon" /></a>
-      </div>
-      <div className="icon">
-        <a href="mailto:benjaminwills057@gmail.com"><FontAwesomeIcon icon={faEnvelope} className="contact-icon" /></a>
-      </div>
+    <div>
+        <div>
+            <h1>Here's how you can contact me:</h1>
+            <p>My name is Ben Wills. I'm a lowly Data Engineer by trade, but thought I would work on some
+                frontend for a change. Here's how you can contact me.
+            </p>
+        </div>
+        <div className="icon-container">
+        <div className="icon">
+            <a href="https://github.com/BenjaminWills" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} style={{ color: 'black' }} className="contact-icon" /></a>
+        </div>
+        <div className="icon">
+            <a href="https://www.linkedin.com/in/benjamin-wills-b22887220/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} style={{ color: '#0077b5' }} className="contact-icon" /></a>
+        </div>
+        <div className="icon">
+            <a href="https://twitter.com" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} style={{ color: '#1DA1F2' }} className="contact-icon" /></a>
+        </div>
+        <div className="icon">
+            <a href="mailto:benjaminwills057@gmail.com"><FontAwesomeIcon icon={faEnvelope} style={{ color: 'black' }} className="contact-icon" /></a>
+        </div>
+        </div>
     </div>
   );
 }
