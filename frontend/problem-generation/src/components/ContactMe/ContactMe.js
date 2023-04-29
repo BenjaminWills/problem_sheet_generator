2e6373bb7b890bb29f170b132c9a7c65c805bd40
Benjaminwills <benjaminwills047@gmail.com>
2023-04-29 16:17:09 +0100

    All links vertically aligned

--- frontend/problem-generation/src/components/ContactMe/ContactMe.js
+++ frontend/problem-generation/src/components/ContactMe/ContactMe.js
@@ -3,19 +3,27 @@ import React from 'react';
 import './ContactMe.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import faMailbox from '@fortawesome/fontawesome-svg-core';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 function ContactMe() {
   return (
-    <div style={{ fontSize: "100px" }} class="social-links">
-    <a href='https://github.com'><FontAwesomeIcon icon={faGithub} /></a>
-    <a href="https://linkedin.com"><FontAwesomeIcon icon={faLinkedin} /></a>
-    <a href="https://instagram.com"><FontAwesomeIcon icon={faInstagram} /></a>
-    <a href="https://twitter.com"><FontAwesomeIcon icon={faTwitter} /></a>
-    <a href="mailto:youremail@example.com"><FontAwesomeIcon icon={faMailbox} /></a>
-</div>
-
-    
+    <div className="icon-container">
+        <div className="icon">
+            <a href='https://github.com'><FontAwesomeIcon icon={faGithub} /></a>
+        </div>
+        <div className="icon">
+            <a href="https://linkedin.com"><FontAwesomeIcon icon={faLinkedin} /></a>
+        </div>
+        <div className="icon">
+            <a href="https://instagram.com"><FontAwesomeIcon icon={faInstagram} /></a>
+        </div>
+        <div className="icon">
+            <a href="https://twitter.com"><FontAwesomeIcon icon={faTwitter} /></a>
+        </div>
+        <div className="icon">
+            <a href="mailto:benjaminwills057@gmail.com"><FontAwesomeIcon icon={faEnvelope} /></a>
+        </div>
+    </div>    
   );
 }
 
