276adbcaa21e70903deea8a2463a86a1f0eb40ab
Benjaminwills <benjaminwills047@gmail.com>
2023-04-29 17:06:11 +0100

    add intro paragraph

--- frontend/problem-generation/src/components/ContactMe/ContactMe.js
+++ frontend/problem-generation/src/components/ContactMe/ContactMe.js
@@ -10,26 +10,27 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 function ContactMe() {
   return (
     <div>
-        <div>
-            <h1>Here's how you can contact me:</h1>
-            <p>My name is Ben Wills. I'm a lowly Data Engineer by trade, but thought I would work on some
-                frontend for a change. Here's how you can contact me.
-            </p>
-        </div>
-        <div className="icon-container">
+      <div>
+        <h1>Here's how you can contact me:</h1>
+        <p>
+          My name is Ben Wills. I'm a lowly Data Engineer by trade, but thought I would work on some
+          frontend for a change. Here's how you can contact me.
+        </p>
+      </div>
+      <div className="icon-container">
         <div className="icon">
-            <a href="https://github.com/BenjaminWills" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} style={{ color: 'black' }} className="contact-icon" /></a>
-        </div>
-        <div className="icon">
-            <a href="https://www.linkedin.com/in/benjamin-wills-b22887220/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} style={{ color: '#0077b5' }} className="contact-icon" /></a>
+          <a href="https://github.com/BenjaminWills" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub} style={{ color: 'black' }} className="contact-icon" /></a>
         </div>
         <div className="icon">
-            <a href="https://twitter.com" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} style={{ color: '#1DA1F2' }} className="contact-icon" /></a>
+          <a href="https://www.linkedin.com/in/benjamin-wills-b22887220/" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} style={{ color: '#0077b5' }} className="contact-icon" /></a>
         </div>
         <div className="icon">
-            <a href="mailto:benjaminwills057@gmail.com"><FontAwesomeIcon icon={faEnvelope} style={{ color: 'black' }} className="contact-icon" /></a>
+          <a href="https://twitter.com" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faTwitter} style={{ color: '#1DA1F2' }} className="contact-icon" /></a>
         </div>
+        <div className="icon">
+          <a href="mailto:benjaminwills057@gmail.com"><FontAwesomeIcon icon={faEnvelope} style={{ color: 'black' }} className="contact-icon" /></a>
         </div>
+      </div>
     </div>
   );
 }
