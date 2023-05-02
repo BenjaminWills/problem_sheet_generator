07e81a069a9959f4a86d7efe8089a720420a0475
Benjaminwills <benjaminwills047@gmail.com>
2023-05-02 21:19:59 +0100

    add unit tests

---
+++ test/unit_tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class MyTest(unittest.TestCase):
+    def test_something(self):
+        # Test code here
+        self.assertEqual(1 + 1, 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
