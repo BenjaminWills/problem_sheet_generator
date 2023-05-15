2b0a5476eb4e90555e53c2faf64e8d520933955f
Benjaminwills <benjaminwills047@gmail.com>
2023-05-15 20:51:48 +0100

    Move blog in navbar

--- frontend/problem-generation/src/components/Navbar/Navbar.js
+++ frontend/problem-generation/src/components/Navbar/Navbar.js
@@ -6,10 +6,10 @@ function Navbar() {
     <nav className="navbar">
       <div className="navbar-logo">Problem sheet generator</div>
       <ul className="navbar-links">
-        <li><a href="/upload">Upload</a></li>
         <li><a href="/problem-generator">Problem Generator</a></li>
-        <li><a href="/contact-me">Contact</a></li>
+        <li><a href="/upload">Upload</a></li>
         <li><a href="/blog">Blog</a></li>
+        <li><a href="/contact-me">Contact</a></li>
       </ul>
     </nav>
   );
