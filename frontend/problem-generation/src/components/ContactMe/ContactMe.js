d6d99f33f2562c96815f80b417ddebe503c48ec3
Benjaminwills <benjaminwills047@gmail.com>
2023-05-15 20:50:36 +0100

    add some styling

--- frontend/problem-generation/src/components/ContactMe/ContactMe.js
+++ frontend/problem-generation/src/components/ContactMe/ContactMe.js
@@ -9,10 +9,17 @@ function ContactMe() {
     <div>
       <div>
         <h1><center>About me</center></h1>
-        <p>
+        <p className = "box-container">
           My name is Ben Wills. I'm a lowly Data Engineer by trade, I'm passionate about mathematics and the
           spreading of all of the interesting things that you can do with it. In this website I wish to display
           how interesting the applications can be.
+        </p>  
+        <br/>
+        <p className = "box-container">
+          I have a first class degree in mathematics from the university of Bristol. I specialised in applied
+          mathematics, my favourite modules included fluid dynamics, probability and statistics and quantum 
+          mechanics! These all showed me how epic the applications of mathematics to the real world really
+          are.
         </p>
         <h2><center>CV</center></h2>
       </div>
